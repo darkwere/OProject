4d80d5ca70c9eae5dc33696a305f86ea25badbcc
AreYouReal <viertual@gmail.com>
2018-09-10 10:59:39 +0300

    Initial commit

---
+++ Source/OProject/OProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPROJECT_API AOProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
